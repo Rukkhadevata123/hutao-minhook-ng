9f7a0219b24ff8d69816ccbdec96082649ad78cd
Rukkhadevata123 <rukkhadevata@foxmail.com>
2025-12-06 19:53:30 +0800

    no need to generate lib every time

--- build.ps1
+++ build.ps1
@@ -1,9 +1,9 @@
-cd src
-cl /c /O2 /TC hutao_seh_stub.c
-lib /OUT:hutao_seh_stub.lib hutao_seh_stub.obj
-del hutao_seh_stub.obj
+# cd src
+# cl /c /O2 /TC hutao_seh_stub.c
+# lib /OUT:hutao_seh_stub.lib hutao_seh_stub.obj
+# del hutao_seh_stub.obj
 
-cd ..
+# cd ..
 $env:RUSTFLAGS="-Lnative=src"
 cargo clippy --fix --allow-dirty --allow-staged
 cargo fmt
